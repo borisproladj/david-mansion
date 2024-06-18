75d60fa4b06b8baf11d164e7a919b94744fa7d5f
Boris <ladjboris@gmail.com>
2024-06-18 19:31:07 +0300

    pulling video from backend

--- app/midcontainer.js
+++ app/midcontainer.js
@@ -1,9 +1,12 @@
 export default function MidContainer() {
+  // URL from Google Cloud Storage
+  const videoUrl = "https://storage.cloud.google.com/davidmansionvideos/video.mp4";
+
   return (
-    <div className="bg-white rounded-lg  shadow-3xl text-black overflow-hidden h-5/6">
+    <div className="bg-white rounded-lg shadow-3xl text-black overflow-hidden h-5/6">
       <video
         className="w-full h-full object-cover hidden xl:block"
-        src="/video.mp4"
+        src={videoUrl}  // Use the video URL here
         controls
         autoPlay
         loop
