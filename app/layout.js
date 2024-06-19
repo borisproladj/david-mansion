597bf75773ab53fb24f44a5dbfd544c90c5b858d
Boris <ladjboris@gmail.com>
2024-06-19 15:45:02 +0300

    added meta data

--- app/layout.js
+++ app/layout.js
@@ -6,15 +6,23 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "King-David Mansion",
-  description: "Made by Boris Ladyjensky",
+  description: "King David Mansion accommodation website made by Boris Ladyjensky",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <Head>
+      <meta name="description" content="A brief description of your page" />
         <link rel="icon" href="/favicon.ico" sizes="any" />
-        <meta property="og:image" content="kingdavidicon.png" />
+        <meta property="og:image" content="https://www.villa-bs.co.il/kingdavidicon.png" />
+        <meta property="og:title" content="King-David Mansion" />
+        <meta property="og:description" content="Explore the luxurious King David Mansion, meticulously designed to offer a unique accommodation experience. Nestled in a serene location, this site features elegant interiors, state-of-the-art amenities, and a captivating ambiance perfect for relaxation and rejuvenation. Discover your ideal getaway and indulge in the exclusivity and privacy that King David Mansion provides." />
+        <meta property="og:url" content="https://www.villa-bs.co.il" />
+        <meta property="og:type" content="website" />
+        <meta property="og:image" content="https://www.villa-bs.co.il/kingdavidicon.png" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
       </Head>
       <body className={inter.className}>{children}</body>
     </html>
